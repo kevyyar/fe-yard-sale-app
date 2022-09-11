0249357969ba27b99251d41f60f954bcc307d442
Kevin Barreto <barretokevin@hotmail.com>
2022-09-11 10:39:55 -0500

    added cart order details and fixed onclick events

--- main.js
+++ main.js
@@ -2,10 +2,25 @@ const mail = document.querySelector(".navbar-email");
 const mailMenu = document.querySelector(".desktop-menu");
 const mobileMenuIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const cart = document.querySelector(".navbar-shopping-cart");
+const cartOrderDetails = document.querySelector(".product-detail");
 
-mail.addEventListener("click", () => toggleMenu(mailMenu));
-mobileMenuIcon.addEventListener("click", () => toggleMenu(mobileMenu));
+mail.addEventListener("click", toggleMailMenu);
+mobileMenuIcon.addEventListener("click", toggleMobileMenu);
+cart.addEventListener("click", toggleCartMenu);
 
-function toggleMenu(element) {
-  element.classList.toggle("inactive");
+function toggleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+  cartOrderDetails.classList.add("inactive");
+}
+
+function toggleMailMenu() {
+  mailMenu.classList.toggle("inactive");
+  cartOrderDetails.classList.add("inactive");
+}
+
+function toggleCartMenu() {
+  cartOrderDetails.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
+  mailMenu.classList.add("inactive");
 }
