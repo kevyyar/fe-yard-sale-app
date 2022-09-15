7ab0a1c58fa1b3396be552493e6d4aa74c266854
Kevin Barreto <barretokevin@hotmail.com>
2022-09-15 08:56:38 -0500

    add grid with products

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const mobileMenuIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
 const cart = document.querySelector(".navbar-shopping-cart");
 const cartOrderDetails = document.querySelector(".product-detail");
+const cardGridContainer = document.querySelector(".cards-container");
 
 mail.addEventListener("click", toggleMailMenu);
 mobileMenuIcon.addEventListener("click", toggleMobileMenu);
@@ -24,3 +25,57 @@ function toggleCartMenu() {
   mobileMenu.classList.add("inactive");
   mailMenu.classList.add("inactive");
 }
+
+const productList = [];
+
+productList.push({
+  name: "Bike",
+  price: 120,
+  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"',
+});
+productList.push({
+  name: "PC",
+  price: 420,
+  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"',
+});
+productList.push({
+  name: "4K TV",
+  price: 1220,
+  img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"',
+});
+
+function renderProducts(productsArr) {
+  for (product of productsArr) {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product-card");
+
+    const productImg = document.createElement("img");
+    productImg.src = product.img;
+
+    const productInfo = document.createElement("div");
+    productInfo.classList.add("product-info");
+
+    const productInfoDiv = document.createElement("div");
+    const productPrice = document.createElement("p");
+    const productName = document.createElement("p");
+    productPrice.innerText = `$${product.price}`;
+    productName.innerText = product.name;
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement("figure");
+    const productInfoIcon = document.createElement("img");
+    productInfoIcon.src = "./icons/bt_add_to_cart.svg";
+    productInfoFigure.appendChild(productInfoIcon);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardGridContainer.appendChild(productCard);
+  }
+}
+
+renderProducts(productList);
